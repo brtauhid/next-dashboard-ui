099fd1b0148cadccfe4e7561f6cd0bd7661fe7f3
brtauhid <br.tauhid@gmail.com>
2024-08-27 07:37:28 +0600

    dashboard menu items and right side content start

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Menu = () => {
 
   const menuItems = [
@@ -116,10 +119,16 @@ const Menu = () => {
   ];
 
   return (
-    <div className=''>
+    <div className='mt-4 text-sm'>
       {menuItems.map(i=>(
-        <div className="" key={i.title}>
-          <span>{i.title}</span>
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4 ">{i.title}</span>
+          {i.items.map(item=>
+            <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start text-gray-500 gap-4 py-2">
+            <Image src={item.icon} alt="" width={20} height={20}/>
+            <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          )}
         </div>
       ))}
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,23 @@
+import Image from "next/image"
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+    <div className='flex items-center justify-between p-4'>
+      {/* Search Bar */}
+      <div className="hidden md:flex">
+        <Image src="/public/search.png" alt="" className="bg-red-700" width={14} height={14}/>
+        <input type="text" placeholder="Search" />
+      </div>
+
+      {/* Icons & User */}
+      <div className="">
+        <div className="">
+          <Image src="/public/message.png" width={20} height={20} alt=""/>
+        </div>
+      </div>
+    </div>
+
+    
   )
 }
 
